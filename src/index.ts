65ea8b8a05d60ccfa406810d15642fa550cbea82
lmis <lionel.miserez@gmail.com>
2021-03-02 20:02:27 +0100

    Drop socket.io

--- src/index.ts
+++ src/index.ts
@@ -1,53 +1,22 @@
 import express from "express";
-import bodyParser from "body-parser";
-import socketIO, { Socket, Server } from "socket.io";
 import cors from "cors";
+import WebSocket from "ws";
 import { hivelingMind } from "./hivelings/mind";
 
 const port = 8000;
-
 const server = express()
   .use(cors({ origin: /\.csb\.app$/ }))
-  .use(bodyParser.json())
-  .use(bodyParser.urlencoded({ extended: true }))
   .listen(port, () => console.log(`Listening on port ${port}`));
 
-enum Signal {
-  // Websockets
-  _CONNECTION = "connection",
-  _DISCONNECT = "disconnect",
+const wss = new WebSocket.Server({ server });
 
-  // App specific things
-  HIVELING_MIND = "hiveling-mind",
-  MIND_VERSION_CHANGED = "hiveling-mind"
-}
-
-const logEvent = (id: string, signal: Signal, payload?: any): void => {
-  console.log(
-    `${id}: ${signal} ${payload ? `(${JSON.stringify(payload)})` : ""}`
-  );
-};
-
-const maxNumberUsers = 1;
-const io = (socketIO as any)(server) as Server;
-io.on(Signal._CONNECTION, (socket: Socket) => {
-  const { id } = socket;
-  const socketIds = [...io.sockets.sockets.values()]
-    .map((s) => s.id)
-    .filter((i) => i !== id);
-  const numberOfSockets = socketIds.length;
-
-  logEvent(id, Signal._CONNECTION, "#" + numberOfSockets);
-  if (numberOfSockets === maxNumberUsers) {
-    socket.disconnect();
-    return;
-  }
-
-  socket.on(Signal._DISCONNECT, () => {});
-
-  socket.on(Signal.HIVELING_MIND, (inputs) => {
+wss.on("connection", (ws) => {
+  console.log("connection");
+  ws.on("message", (message) => {
+    const inputs = JSON.parse(message.toString());
     const outputs = inputs.map(hivelingMind);
-
-    socket.emit(Signal.HIVELING_MIND, outputs);
+    console.log(outputs)
+    ws.send(JSON.stringify(outputs));
   });
 });
+
