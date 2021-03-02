92174ee125c2cb3a75d0ec36c72b9bc9e3e9e11e
lmis <lionel.miserez@gmail.com>
2021-03-02 20:18:48 +0100

    Remove cors

--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,11 @@
 import express from "express";
-import cors from "cors";
 import WebSocket from "ws";
 import { hivelingMind } from "./hivelings/mind";
 
 const port = 8000;
-const server = express()
-  .use(cors({ origin: /\.csb\.app$/ }))
-  .listen(port, () => console.log(`Listening on port ${port}`));
+const server = express().listen(port, () =>
+  console.log(`Listening on port ${port}`)
+);
 
 const wss = new WebSocket.Server({ server });
 
@@ -15,8 +14,6 @@ wss.on("connection", (ws) => {
   ws.on("message", (message) => {
     const inputs = JSON.parse(message.toString());
     const outputs = inputs.map(hivelingMind);
-    console.log(outputs)
     ws.send(JSON.stringify(outputs));
   });
 });
-
