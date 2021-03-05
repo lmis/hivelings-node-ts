ed85ca4881246125a566526ea8b8d38eba4e20ed
lmis <lionel.miserez@gmail.com>
2021-03-05 10:36:15 +0100

    Add "Launch Game" button

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,20 @@ import express from "express";
 import WebSocket from "ws";
 import { hivelingMind } from "./hivelings/mind";
 
+const gameUrl = "https://vc4g6.csb.app/";
+// Hardcoded because I can't figure out the codesandbox magic
+const wssUrl = "wss://zj8h3.sse.codesandbox.io/";
+const redirectUrl = gameUrl + "?hive-mind=" + encodeURIComponent(wssUrl);
 const port = 8000;
-const server = express().listen(port, () =>
-  console.log(`Listening on port ${port}`)
-);
+
+const server = express()
+  .get("/", (_, res) => {
+    // Codesandbox doesn't seem to like redirects, so I send a link instead
+    res.send(
+      `<html><body><a href=${redirectUrl} target="_blank">Launch Game</a></body></html>`
+    );
+  })
+  .listen(port, () => console.log(`Listening on port ${port}`));
 
 const wss = new WebSocket.Server({ server });
 
